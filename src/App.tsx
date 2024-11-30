2c0b36620030164607c0d2f6057664178ec1bb02
araf821 <araf.ahmed200@gmail.com>
2024-11-29 19:42:52 -0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-pink-500">Hello World</h1>;
+  return (
+    <h1 className="w-full max-w-md mx-auto my-20 rounded-md border bg-red-900 text-white font-bold p-12 text-2xl text-center">
+      Hello World
+    </h1>
+  );
 }
 
 export default App;
